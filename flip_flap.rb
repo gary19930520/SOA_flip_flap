3f1b893e2abda91785ff6257a1ff3ec9dcb21f3c
Gary <gary19930520@gmail.com>
2015-10-04 15:32:35 +0800

    Setup
    Func setup and implement take_tsv()

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,6 +1,13 @@
 require_relative 'tsv_buddy'
 
 class FlipFlap
+  include TsvBuddy
   # Do NOT create an initialize method
+  def take_yaml(yaml)
+  	
+  end
 
+  def to_yaml
+  	
+  end
 end
--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -7,7 +7,20 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    line_arr = tsv.split(/\n/) #parse every line
+    list_name = line_arr[0].split("\t")
+    # puts line_arr[0]
+    line_arr.shift # remove first item list_name
+    @data = [] # where to store
+    line_arr.each do |line|
+      values_arr = line.split("\t")
+      tmp_hash = {}
+      list_name.each_index do |index|
+        tmp_hash[list_name[index]] = values_arr[index].chomp
+      end
+      @data.push(tmp_hash)
+    end
+    @data # return
   end
 
   # to_tsv: converts @data into tsv string
